5270bc4513a6c4c251167e18c25a4aba23abfcc7
Manish  Kumar <Amity4933@gmail.com>
2026-01-30 02:57:20 +0530

    setup

---
+++ backend/controller/auth.controllers.js
@@ -0,0 +1,35 @@
+import User from "../models/user.model.js";
+
+const signUp = async (req, res) => {
+  try {
+    const { fullName, email, password, mobile, role } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ message: "User Already Exist." });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "password must be at least 6 characters." });
+    }
+
+    if(mobile.length<10){
+        return res.status(400).json({message:"Mobile Number must be at least 10 digits."})
+    }
+
+    const hashpassword = await bcrypt.hash(password,10)
+
+    user=await User.create({
+        fullName,
+        email,
+        role,
+        mobile,
+        password:hashpassword,
+    })
+  } catch (error) {
+    console.log("");
+    
+  }
+};
---
+++ backend/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose, { Types } from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      Type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+    },
+    mobile: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "owner", deliveryBoy],
+      required: true,
+    },
+  },
+
+  { timestamps: true },
+);
+
+const User=mongoose.model("User",userSchema)
+export default User
